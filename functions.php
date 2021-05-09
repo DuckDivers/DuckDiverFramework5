d6e551917c0b9d3c2ab9a764f7234d721a32dc8a
duckdivers <howard@duckdiverllc.com>
2021-05-09 15:51:05 -0400

    Add Bootstrap SCSS Remove LESS

--- functions.php
+++ functions.php
@@ -118,7 +118,7 @@ endif;
  */
 function dd_theme_scripts() {
 	
-	wp_enqueue_style( '_s-style', get_stylesheet_directory_uri() . '/style.min.css' );
+	wp_enqueue_style( '_s-style', get_template_directory_uri() . '/bootstrap/scss/bootstrap.min.css' );
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
